61795f2bcf1c9574ca7263aab8317359219424bc
tantely <ftantely@gmail.com>
2023-06-06 14:45:51 -0600

    1_Cleaning

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = (props) => (
+  <div>
+    <h1>About</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aut
+      commodi ea, enim, esse excepturi illo nobis obcaecati odit optio
+      praesentium ratione repudiandae unde veritatis vero voluptatem voluptatum.
+      Amet, sunt.
+    </p>
+  </div>
+);
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+        dolorem fugiat odit possimus praesentium suscipit tempora veniam.
+        Aliquid beatae eos est fuga fugiat molestias nihil numquam, officiis
+        recusandae repellat totam!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+        dolorem fugiat odit possimus praesentium suscipit tempora veniam.
+        Aliquid beatae eos est fuga fugiat molestias nihil numquam, officiis
+        recusandae repellat totam!
+      </p>
+    </div>
+  );
+}

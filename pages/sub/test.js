6f5585777a5f3d014c7d8403e8133663e3a18a91
tantely <ftantely@gmail.com>
2023-06-06 14:52:59 -0600

    2_Sub_Folder

---
+++ pages/sub/test.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Test = () => (
+  <div>
+    <h1>Test</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
+      distinctio ducimus esse eum nisi quas saepe sequi sint? Architecto cum
+      dignissimos eum exercitationem non odit quam soluta suscipit. Ad, dolor.
+    </p>
+  </div>
+);
+
+export default Test;

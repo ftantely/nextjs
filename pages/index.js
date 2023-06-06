9fce76af01f71c912665e28cbd0348b3255d7b89
tantely <ftantely@gmail.com>
2023-06-06 15:27:52 -0600

    3_Adding_Other_Components

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
@@ -14,6 +18,7 @@ export default function Home() {
         Aliquid beatae eos est fuga fugiat molestias nihil numquam, officiis
         recusandae repellat totam!
       </p>
+      <Footer />
     </div>
   );
 }

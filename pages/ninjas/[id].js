576b1f70df0290305eae3e7f9c5c19b8bd266367
tantely <ftantely@gmail.com>
2023-06-09 14:53:05 -0600

    14_Fetching_Single_Item

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,24 +1,38 @@
+/* getStaticPaths tells Next how many Html pages need to be made
+ based on our data */
+
 export const getStaticPaths = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
-
-  // map data to an array of path objects with params (id)
   const paths = data.map((ninja) => {
     return {
       params: { id: ninja.id.toString() },
     };
   });
-
   return {
     paths,
     fallback: false,
   };
 };
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+
+  const data = await res.json();
+
+  return {
+    props: { ninja: data },
+  };
+};
+
+const Details = ({ ninja }) => {
   return (
     <div>
-      <h1>Details Page</h1>
+      <h1>{ninja.name}</h1>
+      <p>{ninja.email}</p>
+      <p>{ninja.website}</p>
+      <p>{ninja.address.city}</p>
     </div>
   );
 };
